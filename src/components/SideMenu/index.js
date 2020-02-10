992205935aa6814afccef5ac0715d9311c896b59
vchrpng <vchrpng@gmail.com>
2020-02-10 12:49:46 +0700

    be able to change header title

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -4,6 +4,8 @@ import { fetchAlbumRequest } from '../../actions/album'
 import { fetchArtistRequest } from '../../actions/artist'
 import { fetchRecentlyPlayedRequest } from '../../actions/song'
 import { fetchSongsRequest } from '../../actions/song'
+import { updateHeaderTitle } from '../../actions/ui'
+import { bindActionCreators } from 'redux'
 
 const SideMenu = ({
 	token,
@@ -11,11 +13,12 @@ const SideMenu = ({
 	fetchAlbumRequest,
 	fetchArtistRequest,
 	fetchRecentlyPlayedRequest,
-	fetchSongsRequest
+	fetchSongsRequest,
+	updateHeaderTitle
 }) => {
 
 	const handleClick = (name)  => {
-		// updateHeaderTitle(name)
+		updateHeaderTitle(name)
 		// updateViewType(name)
 	}
 
@@ -71,10 +74,12 @@ const SideMenu = ({
 
 export default connect(state => ({
 	token : state.tokenReducer.token ? state.tokenReducer.token : '',
-	artistIds : state.artistReducer.artistIds
-}),{
+	artistIds : state.artistReducer.artistIds,
+	title: state.uiReducer.title
+}), dispatch => bindActionCreators({
+	updateHeaderTitle,
 	fetchAlbumRequest,
 	fetchArtistRequest,
 	fetchSongsRequest,
 	fetchRecentlyPlayedRequest
-})(SideMenu)
+}, dispatch))(SideMenu)
