1f81ae1856a84dd50d7b22649684c63f5425e9e0
vchrpng <vchrpng@gmail.com>
2020-02-16 12:59:25 +0700

    update songlist component

--- src/components/SongList/index.js
+++ src/components/SongList/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const SongList = () => {
-    return (
-        <div>SongList</div>
-    )
+
+class SongList extends React.Component {
+
+    renderSongs = () => {
+        return this.props.songs.map((song, i) => (
+                <li>
+                    <div>
+                        <p>{song.track.name}</p>
+                    </div>
+        
+                    <div>
+                        <p>{song.track.artists[0].name}</p>
+                    </div>
+                    <div>
+                        <p>{song.track.album.name}</p>
+                    </div>
+                </li>
+        ))
+    }
+    render() {
+    console.log('song list',this.props)
+        return (
+            <div>
+                <h2>song list</h2>
+                {this.props.songs && this.renderSongs()}
+            </div>
+        )
+    }
 }
 
-export default SongList
+
+
+export default connect(state => ({
+    token: (state.tokenReducer && state.tokenReducer.token) || '',
+    songs: (state.songsReducer && state.songsReducer.songs) || '',
+    viewType: state.uiReducer.viewType,
+}),{})(SongList)
