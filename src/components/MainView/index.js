edc66fa08431501da4f0295dd5b86ca1568e730c
vchrpng <vchrpng@gmail.com>
2020-03-01 14:38:55 +0700

    try to make main view up to date

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { connect } from 'react-redux'
-import SongList from "../SongList";
 import AlbumList from "../AlbumList";
 import ArtistList from "../ArtistList";
-import BrowseView from "../BrowseView";
 
 const MainView = ({ headerTitle }) => {
   return (
     <React.Fragment>
-      {headerTitle === "Albums" ? (
         <AlbumList  />
-      ) : headerTitle === "Artists" ? (
+
         <ArtistList />
-      ) : headerTitle === "Browse" ? (
-        <BrowseView />
-      ) : (
-        <SongList />
-      )}
     </React.Fragment>
   )
 }
