f8147b7a0e96aee901d60a04a8d5b8eef8b2b0a1
vchrpng <vchrpng@gmail.com>
2020-03-17 20:02:52 +0700

    make layout for now playing bar

--- src/components/NowPlayingBar/index.js
+++ src/components/NowPlayingBar/index.js
@@ -3,7 +3,18 @@ import React from 'react'
 const NowPlayingBar = () => {
     return (
         <footer style={{ height: '90px' }}>
-            Now PlayingBar
+            <div style={{ padding: '0 16px'}}>
+                <div style={{ width: '30%'}}>
+                    left
+                </div>
+                <div  style={{ width: '40%'}}>
+                        center
+                </div>
+                <div  style={{ width: '30%'}}>
+                            right
+                </div>
+            </div>
+          
         </footer>
     )
 }
