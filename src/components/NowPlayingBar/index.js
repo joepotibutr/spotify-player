1239cb4ebffb4200e18b9a0984e8923f70793720
vchrpng <vchrpng@gmail.com>
2020-03-17 20:28:06 +0700

    be able to get latest songs played

--- src/components/NowPlayingBar/index.js
+++ src/components/NowPlayingBar/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const NowPlayingBar = () => {
-    return (
-        <footer style={{ height: '90px' }}>
-            <div style={{ padding: '0 16px'}}>
-                <div style={{ width: '30%'}}>
-                    left
+class NowPlayingBar extends React.Component {
+    render() {
+        const lastSongPlayed = this.props.recentlySongs.length && this.props.recentlySongs[0].track
+        console.log(this.props.recentlySongs.length && this.props.recentlySongs)
+
+        return (
+            <footer style={{ height: '90px' }}>
+                <div style={{ padding: '0 16px', display: 'flex'}}>
+                    <div style={{ width: '30%'}}>
+                        {lastSongPlayed ? 
+                            <img src={lastSongPlayed.album.images[2].url} /> : null
+                        }
+                    </div>
+                    <div  style={{ width: '40%'}}>
+                            center
+                    </div>
+                    <div  style={{ width: '30%'}}>
+                                right
+                    </div>
                 </div>
-                <div  style={{ width: '40%'}}>
-                        center
-                </div>
-                <div  style={{ width: '30%'}}>
-                            right
-                </div>
-            </div>
-          
-        </footer>
-    )
+            </footer>
+        )
+    }
 }
 
-export default NowPlayingBar
+export default connect(state => ({
+    recentlySongs: (state.songReducer && state.songReducer.recentlySongs) || '',
+}))(NowPlayingBar)
