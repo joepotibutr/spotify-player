ce785f43c5181d427495acec0362411f709fc703
vchrpng <vchrpng@gmail.com>
2020-03-26 20:35:55 +0700

    style installing app bar

--- src/components/Artwork/index.js
+++ src/components/Artwork/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
+const DownloadIcon = require('../../images/download.svg')
+
 const Artwork = albumArtwork => (
-    <div>
-        img
+    <div style={{ display: 'flex' }}>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            filter: 'invert(1)',
+            marginRight: '15px'
+        }}>
+            <img style={{
+                height: '15px'
+            }} src={DownloadIcon} />
+        </div>
+       <p><strong>Install App</strong></p>
     </div>
 )
 
