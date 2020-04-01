04b19ee747592436c775f28592a2147d83a74e1b
vchrpng <vchrpng@gmail.com>
2020-04-01 16:54:59 +0700

    show playlist thumbnail

--- src/components/PlaylistView/index.js
+++ src/components/PlaylistView/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const PlaylistView = ({ headerTitle }) => {
-    return (
-        <div style={{ background: 'white'}}>
-            <h1>{headerTitle}</h1>
-        </div>
-    )
+class PlaylistView extends React.Component {
+
+    render() {
+        const currentPlaylist = this.props.playlists && this.props.playlists.find(playlist => 
+            playlist.name === this.props.headerTitle)
+            console.log('currentPlaylist',currentPlaylist)
+        return (
+            <div style={{ background: 'white'}}>
+                <div>
+                    <div style={{
+                        minWidth: '180px',
+                        minHeight: '180px'
+                    }}>
+                        <img style={{
+                        width: '180px',
+                        height: '180px'
+                    }} 
+                    src={currentPlaylist.images[0].url}/>
+                    </div>
+                    <div><h1>{this.props.headerTitle}</h1></div>
+                </div>
+                <div>
+                    <ul></ul>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default connect(state => ({
