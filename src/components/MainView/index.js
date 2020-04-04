4de36f178bcc6759c44accd4b9cfe779ca33d57b
vchrpng <vchrpng@gmail.com>
2020-04-04 08:55:39 +0700

    prepare scroll up after rerender

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -4,8 +4,16 @@ import ArtistList from "../ArtistList";
 import PlaylistView from '../PlaylistView'
 import { viewType } from '../../constants'
 
-const MainView = ({ headerTitle }) => {
-  if (headerTitle === viewType.RECENTLY_PLAYED) {
+class MainView extends React.Component {
+
+  // componentWillReceiveProps(nextProps) {
+  //   if (this.props.headerTitle !== nextProps.headerTitle) {
+  //     window.scrollTo(0, 0)
+  //   }
+  // }
+
+ render() {
+  if (this.props.headerTitle === viewType.RECENTLY_PLAYED) {
     return (
       <div style={{ padding: '0 32px', height: 'calc(100vh - 90px)', overflowY: 'scroll' }}>
           <div style={{ position: 'fixed ', background: 'rgb(18, 18, 18)', width: '100%', height: '60px'}}>
@@ -28,6 +36,7 @@ const MainView = ({ headerTitle }) => {
       </div>
       )
   }
+ }
  
 }
 
