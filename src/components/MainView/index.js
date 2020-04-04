11012f2c7f5fca8fee3711936ab4c8298a4b6e0f
vchrpng <vchrpng@gmail.com>
2020-04-04 09:10:10 +0700

    refactor

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -12,32 +12,26 @@ class MainView extends React.Component {
   //   }
   // }
 
+  renderMainView = () => {
+    switch (this.props.headerTitle) {
+      case viewType.RECENTLY_PLAYED :
+        return <ArtistList />
+      default: 
+        return <PlaylistView />
+    }
+  }
+
  render() {
-  if (this.props.headerTitle === viewType.RECENTLY_PLAYED) {
     return (
       <div style={{ padding: '0 32px', height: 'calc(100vh - 90px)', overflowY: 'scroll' }}>
-          <div style={{ position: 'fixed ', background: 'rgb(18, 18, 18)', width: '100%', height: '60px'}}>
-            <span>{`<`}</span>
-            <span>{`>`}</span>
-          </div>
-          <ArtistList />
+        <div style={{ position: 'fixed ', background: 'rgb(18, 18, 18)', width: '100%', height: '60px'}}>
+          <span>{`<`}</span>
+          <span>{`>`}</span>
+          {this.renderMainView()}
+        </div>
       </div>
     )
-  } else {
-      return (
-        <div style={{ padding: '0 32px', height: 'calc(100vh - 90px)', overflowY: 'scroll' }}>
-          <div style={{ position: 'fixed ', background: 'rgb(18, 18, 18)', width: '100%', height: '60px'}}>
-            <span>{`<`}</span>
-            <span>{`>`}</span>
-          </div>
-          <div style={{ height: '900px',paddingTop: '60px'}}>
-          <PlaylistView />
-          </div>
-      </div>
-      )
-  }
  }
- 
 }
 
 
