f73d9f13fb3b54ffeb5a786ba66d597e8f04af04
vchrpng <vchrpng@gmail.com>
2020-04-06 15:59:09 +0700

    refactor directory

---
+++ src/components/NowPlayingBar/TrackPlayerSection/index.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { play,stop,pause,resume } from '../../../actions/player'
+
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+
+
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+const ShuffleIcon = require('../../../images/change.svg')
+const RepeatIcon = require('../../../images/repeat.svg')
+
+export const IconWrapper = styled.div` 
+width: 32px; 
+height: 32px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+
+class TrackPlayerSection extends React.Component {
+    
+    render() {
+        const { play,pause,resume,stop, songPlaying, currentlyPlaying,recentlySongs } = this.props
+
+        const lastSongPlayed = recentlySongs.length && recentlySongs[0]
+
+        const currentTrack = currentlyPlaying ? currentlyPlaying : lastSongPlayed
+        return (
+            <div  style={{ 
+                width: '40%',
+                flexDirection: 'column',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                maxWidth: '722px'
+            }}>
+                <div style={{ 
+                    marginBottom: '12px',
+                    width: '224px',
+                    justifyContent: 'space-between',
+                    flexDlow: 'row nowrap',
+                    display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+                    <IconWrapper><span><img src={ShuffleIcon} style={{ width: '1em', }} /></span></IconWrapper>
+                    <IconWrapper><span><SkipPreviousIcon /></span></IconWrapper>
+                    {songPlaying ? <IconWrapper onClick={pause}><span><PauseCircleOutlineIcon /></span></IconWrapper>
+                     : <IconWrapper onClick={() => play(currentTrack)}><span><PlayArrowIcon /></span></IconWrapper>}
+                    <IconWrapper><span><SkipNextIcon /></span></IconWrapper>
+                    <IconWrapper><span><img src={RepeatIcon} style={{ width: '1em'}} /></span></IconWrapper>
+                </div>
+
+                <div style={{ display: 'flex',width: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontFamily:  `'Varela Round', sans-serif `,
+                }}>
+                    <div style={{
+                        fontSize: '11px',
+                        lineHeight: '16px',
+                        letterSpacing: '.015em',
+                        minWidth: '40px',
+                        textAlign: 'center',
+                    }}>2:49</div>
+                    <div style={{
+                        height: '12px',
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                        <div style={{
+                            backgroundColor: '#404040',
+                            borderRadius: '2px',
+                            display: 'flex',
+                            height: '4px',
+                            width: '100%',
+                        }}></div>
+                    </div>
+                    <div style={{
+                        fontSize: '11px',
+                        lineHeight: '16px',
+                        letterSpacing: '.015em',
+                        minWidth: '40px',
+                        textAlign: 'center',
+                    }}>0:34</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default connect(state => ({
+    recentlySongs: (state.songReducer && state.songReducer.recentlySongs) || '',
+    songPlaying: (state.songReducer && state.playerReducer.songPlaying) || '',
+    currentlyPlaying: (state.songReducer && state.playerReducer.currentlyPlaying) || '',
+}),dispatch => bindActionCreators({
+	play,pause,stop,resume
+}, dispatch))(TrackPlayerSection)
--- src/components/NowPlayingBar/index.js
+++ src/components/NowPlayingBar/index.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { play,stop,pause,resume } from '../../actions/player'
+
+import TrackPlayerSection from './TrackPlayerSection'
+
 import PictureInPictureAltIcon from '@material-ui/icons/PictureInPictureAlt';
 
 import LikeIcon from '@material-ui/icons/Favorite';
 // import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 
-
-import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import DevicesIcon from '@material-ui/icons/Devices';
@@ -22,15 +18,15 @@ import VolumeUpIcon from '@material-ui/icons/VolumeUp'
 // import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 // import AddIcon from '@material-ui/icons/Add';
 
-const ShuffleIcon = require('../../images/change.svg')
-const RepeatIcon = require('../../images/repeat.svg')
+
 
 export const IconWrapper = styled.div` 
 width: 32px; 
 height: 32px;
 display: flex;
 justify-content: center;
-align-items: center;`
+align-items: center;
+`
 
 
 class NowPlayingBar extends React.Component {
@@ -41,7 +37,7 @@ class NowPlayingBar extends React.Component {
     //   }
 
     render() {
-        const { play,pause,resume,stop, songPlaying, currentlyPlaying,recentlySongs } = this.props
+        const { currentlyPlaying,recentlySongs } = this.props
 
         const lastSongPlayed = recentlySongs.length && recentlySongs[0]
 
@@ -118,64 +114,7 @@ class NowPlayingBar extends React.Component {
                             <div><span><PictureInPictureAltIcon style={{ width: '15px '}} /></span></div>
                         </div>
                     </div>
-                    <div  style={{ 
-                        width: '40%',
-                        flexDirection: 'column',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        maxWidth: '722px'
-                    }}>
-                        <div style={{ 
-                            marginBottom: '12px',
-                            width: '224px',
-                            justifyContent: 'space-between',
-                            flexDlow: 'row nowrap',
-                            display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-                            <IconWrapper><span><img src={ShuffleIcon} style={{ width: '1em', }} /></span></IconWrapper>
-                            <IconWrapper><span><SkipPreviousIcon /></span></IconWrapper>
-                            {songPlaying ? <IconWrapper onClick={pause}><span><PauseCircleOutlineIcon /></span></IconWrapper>
-                             : <IconWrapper onClick={() => play(currentTrack)}><span><PlayArrowIcon /></span></IconWrapper>}
-                            <IconWrapper><span><SkipNextIcon /></span></IconWrapper>
-                            <IconWrapper><span><img src={RepeatIcon} style={{ width: '1em'}} /></span></IconWrapper>
-                        </div>
-
-                        <div style={{ display: 'flex',width: '100%',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            fontFamily:  `'Varela Round', sans-serif `,
-                        }}>
-                            <div style={{
-                                fontSize: '11px',
-                                lineHeight: '16px',
-                                letterSpacing: '.015em',
-                                minWidth: '40px',
-                                textAlign: 'center',
-                            }}>2:49</div>
-                            <div style={{
-                                height: '12px',
-                                width: '100%',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                            }}>
-                                <div style={{
-                                    backgroundColor: '#404040',
-                                    borderRadius: '2px',
-                                    display: 'flex',
-                                    height: '4px',
-                                    width: '100%',
-                                }}></div>
-                            </div>
-                            <div style={{
-                                fontSize: '11px',
-                                lineHeight: '16px',
-                                letterSpacing: '.015em',
-                                minWidth: '40px',
-                                textAlign: 'center',
-                            }}>0:34</div>
-                        </div>
-                    </div>
+                    <TrackPlayerSection />
                     <div  style={{ 
                         width: '30%',
                         minWidth: '180px', 
@@ -227,6 +166,4 @@ export default connect(state => ({
     recentlySongs: (state.songReducer && state.songReducer.recentlySongs) || '',
     songPlaying: (state.songReducer && state.playerReducer.songPlaying) || '',
     currentlyPlaying: (state.songReducer && state.playerReducer.currentlyPlaying) || '',
-}),dispatch => bindActionCreators({
-	play,pause,stop,resume
-}, dispatch))(NowPlayingBar)
+}))(NowPlayingBar)
