e2bd058964ad2e38d422d7adb2a42c678027b46d
vchrpng <vchrpng@gmail.com>
2020-04-26 15:16:18 +0700

    refactor

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -13,47 +13,31 @@ const MainViewLayout = styled.div`
 `
 
 class MainView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        opacity: 0,
-    };
-  } 
+    constructor(props) {
+      super(props);
+      this.state = {
+          opacity: 0,
+      };
+    } 
 
 
-onScroll = (e) => {
-    if (e.target.scrollTop < 400) {
-        const opacity = e.target.scrollTop / 300
-        this.setState({
-            opacity
-        })
-    }
-}
+  onScroll = (e) => {
+      if (e.target.scrollTop < 400) {
+          const opacity = e.target.scrollTop / 300
+          this.setState({
+              opacity
+          })
+      }
+  }
 
-componentDidMount() {
-    window.addEventListener('scroll', this.onScroll,true);
-}
+  componentDidMount() {
+      window.addEventListener('scroll', this.onScroll,true);
+  }
 
-componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll);
-}
-  
+  componentWillUnmount() {
+      window.removeEventListener('scroll', this.onScroll);
+  }
 
-  // async componentWillReceiveProps(nextProps) {
-  //   // if (this.props.headerTitle !== nextProps.headerTitle) {
-  //   //   window.scrollTo(0, 0)
-  //   // }
-
-  //   if(nextProps.token) {
-  //     const {data} = await axios.get('https://api.spotify.com/v1/me/player/devices',{ 
-  //     headers : { 'Authorization': 'Bearer ' + nextProps.token }
-  //   })
-  //     const res = await axios.get('https://api.spotify.com/v1/recommendations/available-genre-seeds',{ 
-  //     headers : { 'Authorization': 'Bearer ' + nextProps.token }
-  // })
-  // console.log('device=',data,res)
-  //   }
-  // }
 
 
 
@@ -69,15 +53,13 @@ componentWillUnmount() {
  render() {
     return (
       <MainViewLayout>
-        <div style={{ opacity: this.state.opacity, zIndex:3, position: 'fixed ', background: 'rgb(18, 18, 18)', width: '100%', height: '80px'}}>
-          <span>{`<`}</span>
-          <span>{`>`}</span>
-          {this.state.opacity}
-          <button onClick={() => {
-            window.location.href = 'https://www.spotify.com/logout/'
-            // window.location.href = 'http://localhost:3000'
-            }}>Logout</button>
-        </div>
+        <header style={{ opacity: this.state.opacity, zIndex:3, position: 'fixed ', background: 'rgb(18, 18, 18)', width: '100%', height: '80px'}}>
+          <div>
+            <span>{`<`}</span>
+            <span>{`>`}</span>
+          </div>
+          <div>User</div>
+        </header>
           {this.renderMainView()}
       </MainViewLayout>
     )
