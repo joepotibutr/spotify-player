9cc809b077e3bdf76801f48033a6faeff23405d2
vchrpng <vchrpng@gmail.com>
2020-04-26 17:20:10 +0700

    update main header style

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,24 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import ArrowDropDownIcon from   '@material-ui/icons/ArrowDropDown'
+
+
+const HeaderLayout = styled.header`
+    display:flex;
+    opacity: ${props => props.opacity}; 
+    z-index:3; 
+    position: fixed ; 
+    background: rgb(18, 18, 18);
+    width: 100%;
+    height: 80px;
+`
+
 
 const UserIcon = require('../../images/user.svg')
 
 const MainHeader = ({ opacity }) => {
     return (
-        <header style={{ 
-            display:'flex',
-            opacity: opacity, 
-            zIndex:3, 
-            position: 'fixed ', 
-            background: 'rgb(18, 18, 18)', 
-            width: '100%', 
-            height: '80px'
-            }}>
+        <HeaderLayout opacity={opacity}>
             <div>
                 <span>{`<`}</span>
                 <span>{`>`}</span>
             </div>
-            <div style={{ display: 'flex' }}>User<img style={{ width: '30px', height: '30px' }} src={UserIcon} /></div>
-        </header>
+            <div className="current-user"style={{ display: 'flex' }}>
+                User
+                <img style={{ width: '30px', height: '30px' }} src={UserIcon} />
+                <span>CurrentUser</span>
+                <div><ArrowDropDownIcon /></div>
+            </div>
+        </HeaderLayout>
     )
 }
 
