7a30242f325354929f7212b7218d52800d0ff546
vchrpng <vchrpng@gmail.com>
2020-04-30 19:44:33 +0700

    create user liked songs component

---
+++ src/components/UserLibrary/UserLikedSongs/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class UserLikedSongs extends React.Component {
+    render(){
+        return (
+            <div>User Liked Songs</div>
+        )
+    }
+}
+
+
+export default UserLikedSongs
