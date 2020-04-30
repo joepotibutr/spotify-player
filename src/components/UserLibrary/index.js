987c5bb1b193e0df8a6cd4ed46a7d496261618ae
vchrpng <vchrpng@gmail.com>
2020-04-30 18:08:41 +0700

    styling new component

--- src/components/UserLibrary/index.js
+++ src/components/UserLibrary/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { UserLibraryLayout } from './style'
 
 class UserLibrary extends React.Component {
 
     render() {
         return (
-            <div>User Library</div>
+            <UserLibraryLayout>User Library</UserLibraryLayout>
         )
     }
 }
