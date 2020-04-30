a742e9837280290a2fb3797d77c78216018e3d5e
vchrpng <vchrpng@gmail.com>
2020-04-30 18:08:18 +0700

    create stylesheet for new component

---
+++ src/components/UserLibrary/style.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+
+export const UserLibraryLayout = styled.div`
+height: 900px;
+margin-top: 90px;
+padding: 0 32px;
+`
+
+export const CollectionItem = styled.li`
+    overflow:hidden;
+    cursor:pointer;
+    background: #282828;
+    border-radius: 8px;
+    padding: 20px 20px 16px;
+    img {
+        width: 100%;
+        height: auto;
+        border-radius:50%;
+        box-shadow: 0 10px 30px 0 rgba(0,0,0,.3), 0 1px 2px 0 rgba(0,0,0,.2)
+    }
+    .play-btn {
+        position:absolute;
+        justify-content:center;
+        align-items:center;
+        width: 50px;
+        height: 50px;
+        background:#1db954;
+        display:none;
+        border-radius:50%;
+        transform: translateX(90px);
+    }
+    &:hover {
+        .play-btn {
+            display:flex;
+        }
+    }
+`
