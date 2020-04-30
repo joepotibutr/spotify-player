f5996f0848e823ce166526169eb8c3b24d977d84
vchrpng <vchrpng@gmail.com>
2020-04-30 20:24:56 +0700

    render collection each item

--- src/components/UserLibrary/index.js
+++ src/components/UserLibrary/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { UserLibraryLayout } from './style'
 import { libraryView } from '../../constants'
+// import { fetchArtist } from '../../actions/artist'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 class UserLibrary extends React.Component {
     constructor(props) {
@@ -10,16 +12,69 @@ class UserLibrary extends React.Component {
         }
     }
 
+
+
+    renderViewType = () => {
+        console.log('props',this.props)
+
+        switch(this.props.library) {
+            case libraryView.PLAYLISTS :
+                console.log('fire')
+                // this.renderCollectionItem(this.props.playlists)
+               return this.props.playlists && this.props.playlists.map((i,o) =>{
+                    console.log(i)
+                    return (<h1>Hey</h1>)
+                })
+            break;
+            default :
+               return null
+        }
+    }
+
+    renderCollectionItem = (items) => {
+        console.log('items',items)
+        return items && items.map((item, i) => 
+                <li key={i}>
+                    <div style={{
+                        marginBottom: '16px'
+                    }}>
+                       {item.images.length ?  <img  src={item.images[0].url}/> : 'blank'}
+                    </div>
+                    <div>
+                        <div className="play-btn"><PlayArrowIcon /></div>
+                        <div>
+                            <h4 style={{
+                                fontSize: '14px',
+                                lineHeight: '20px',
+                                fontWeight: 700,
+                                letterSpacing: '.24px',
+                                color: '#fff'
+                            }}>{item.name}</h4>
+                        </div>
+                        <div>
+                            <span>{item.type.charAt(0).toUpperCase() + item.type.slice(1)}</span>
+                        </div>
+                    </div>
+                   
+                </li>)
+    }
+
     render() {
         return (
         <UserLibraryLayout>
             <h3>{this.props.library}</h3>
-
+            <ul style={{
+                    display: 'grid',
+                    gridGap: '16px',
+                    gridTemplateColumns: 'repeat(auto-fill,minmax(164px,1fr))'
+                }}>{this.renderViewType()}
+            </ul>
         </UserLibraryLayout>
         )
     }
 }
 
 export default connect(state => ({
+    playlists: state.playlistReducer.playlists,
     library: state.uiReducer.library 
 }))(UserLibrary)
