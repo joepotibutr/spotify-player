3de33e24f864bbf1e01ed09b40ef03b47399207a
vchrpng <vchrpng@gmail.com>
2020-05-02 16:35:25 +0700

    update library header styles

--- src/components/MainHeader/LibraryNavigation/index.js
+++ src/components/MainHeader/LibraryNavigation/index.js
@@ -3,10 +3,17 @@ import styled from 'styled-components'
 
 
 const LibraryNavStyleWrapper = styled.div`
+    padding:10px;
+    background: ${props => props.active ? '#282828' : 'transparent'};
+    border-radius: 4px;
+    
     .nav-button {
         outline:0;
-    background: ${props => props.active ? 'red' : 'black'};
-
+        border:0;
+        background:transparent;
+        color: #fff;
+        font-weight: bold;
+        font-family: "Varela Round", sans-serif;
     }
 
 `
