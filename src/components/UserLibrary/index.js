bcb971553321868c23b126404f1f48bf18cda46e
vchrpng <vchrpng@gmail.com>
2020-05-07 15:11:55 +0700

    feat(UserLibrary): display playlist tracks

--- src/components/UserLibrary/index.js
+++ src/components/UserLibrary/index.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { UserLibraryLayout } from './style'
 import { libraryView } from '../../constants'
 import { fetchSongs } from '../../actions/song'
+import { updateHeaderTitle } from '../../actions/ui'
+import { fetchPlaylistSongsRequest } from '../../actions/playlist';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 import { CollectionItem } from './style'
@@ -20,6 +22,15 @@ class UserLibrary extends React.Component {
         if (nextProps.token && !nextProps.songs.length) {
             this.props.fetchSongs(nextProps.token)
         }
+    }
+
+    handlePlaylistClick = (playlist) => {
+            this.props.fetchPlaylistSongsRequest(
+              playlist.owner.id,
+              playlist.id,
+              this.props.token
+            )
+            this.props.updateHeaderTitle(playlist.name);
     }
 
     renderViewType = () => {
@@ -40,7 +51,7 @@ class UserLibrary extends React.Component {
         return items && items.map((item, i) => {
             const collectionDetail = item.description || `By ${item.owner.display_name}`
             
-            return <CollectionItem key={i}>
+            return <CollectionItem onClick={() => this.handlePlaylistClick(item)} key={i}>
                         <div className="cover-art">
                             {item.images.length ?  <img alt="cover" className="item-cover" src={item.images[0].url}/> : 
                             <div className="empty-playlist">
@@ -78,5 +89,8 @@ export default connect(state => ({
     playlists: state.playlistReducer.playlists,
     library: state.uiReducer.library 
 }), (dispatch) => ({
-    fetchSongs: (accessToken) => dispatch(fetchSongs(accessToken))
+    updateHeaderTitle: (title) => dispatch(updateHeaderTitle(title)),
+    fetchSongs: (accessToken) => dispatch(fetchSongs(accessToken)),
+    fetchPlaylistSongsRequest: (ownerId,playlistId,token) => 
+        dispatch(fetchPlaylistSongsRequest(ownerId, playlistId,token))
 }) )(UserLibrary)
