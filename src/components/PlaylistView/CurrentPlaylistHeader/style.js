037bd05d37ff89be7e1d5b7216572654fa862af7
vchrpng <vchrpng@gmail.com>
2020-05-11 14:48:18 +0700

    style(PlaylistView): change gradient color

--- src/components/PlaylistView/CurrentPlaylistHeader/style.js
+++ src/components/PlaylistView/CurrentPlaylistHeader/style.js
@@ -5,7 +5,7 @@ export const LikedSongsCoverArt = styled.div`
     min-width:190px;
     width: 17vw;
     height: auto;
-    background: linear-gradient(158deg, rgba(70,12,245,1) 0%, rgba(142,141,230,1) 100%);
+    background: linear-gradient(158deg, #431eb3, #b4acd4 100%);
     display:flex;
     justify-content:center;
     align-items:center;
