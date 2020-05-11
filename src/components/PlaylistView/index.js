6d1e3f18e6e4881cfbf978c0bee850ffca543673
vchrpng <vchrpng@gmail.com>
2020-05-11 14:47:22 +0700

    style(PlaylistView): get color from image

--- src/components/PlaylistView/index.js
+++ src/components/PlaylistView/index.js
@@ -7,7 +7,7 @@ import { PlaylistViewLayout } from './style'
 
 import CurrentPlaylistHeader from './CurrentPlaylistHeader'
 import PlaylistTracklist from './PlaylistTracklist'
-
+import { Palette } from 'react-palette';
 
 
 class PlaylistView extends React.Component {
@@ -17,30 +17,38 @@ class PlaylistView extends React.Component {
         const isUserLikedSongs = headerTitle === viewType.LIKED_SONGS
         const currentPlaylist =  playlists && playlists.find(playlist => 
             playlist.name === headerTitle)
-           
+        
+        const playlistImageUrl = isUserLikedSongs ? '' : currentPlaylist.images[0].url
         return (
-            <PlaylistViewLayout>
-                <CurrentPlaylistHeader
-                    isUserLikedSongs={isUserLikedSongs} 
-                    currentPlaylist={currentPlaylist}
-                    headerTitle={headerTitle}
-                />
-                 <div className="playlist-actions"> 
-                    <div className="button-wrapper">
-                        <div className="play-button">
-                            <button onClick={() => this.props.onPlay()}>PLAY</button>
+            <Palette src={playlistImageUrl}>
+                {({data, loading }) => {
+                    return loading ? <h2>LOADING</h2> :
+                     (<PlaylistViewLayout backgroundColor={isUserLikedSongs ? '#431eb3' : data.vibrant}>
+                        <CurrentPlaylistHeader
+                            isUserLikedSongs={isUserLikedSongs} 
+                            currentPlaylist={currentPlaylist}
+                            headerTitle={headerTitle}
+                        />
+                        <div className="playlist-actions"> 
+                            <div className="button-wrapper">
+                                <div className="play-button">
+                                    <button onClick={() => this.props.onPlay()}>PLAY</button>
+                                </div>
+                                <div>.....</div>
+                            </div>
                         </div>
-                        <div>.....</div>
-                    </div>
-                </div>
-                <div className="grid-area-tracks">
-                    {this.props.songs && 
-                        <PlaylistTracklist 
-                            onPlay={this.props.onPlay} 
-                            songs={this.props.songs}
-                        />}
-                </div>
-            </PlaylistViewLayout>
+                        <div className="grid-area-tracks">
+                            {this.props.songs && 
+                                <PlaylistTracklist 
+                                    onPlay={this.props.onPlay} 
+                                    songs={this.props.songs}
+                                />}
+                        </div>
+                    </PlaylistViewLayout>)
+                }
+                }
+            </Palette>
+            
         )
     }
 }
