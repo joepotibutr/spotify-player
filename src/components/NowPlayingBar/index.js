6dc083292ef46d5b10d1c62d1915a3f72771731a
vchrpng <vchrpng@gmail.com>
2020-05-15 15:54:32 +0700

    refactor(NowPlaying): refactor

--- src/components/NowPlayingBar/index.js
+++ src/components/NowPlayingBar/index.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
-
+import CurrentTrack from './CurrentTrack'
 import { fetchRecentlyPlayedSongs } from '../../actions/song'
 
 import TrackPlayerSection from './TrackPlayerSection'
 import ProgressBar from '../ProgressBar'
 
-import PictureInPictureAltIcon from '@material-ui/icons/PictureInPictureAlt';
-
-import LikeIcon from '@material-ui/icons/Favorite';
 // import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-
-import { NowPlayingWrapper, CurrentTrackSection, EllipsisText } from './style'
+import { NowPlayingWrapper } from './style'
 
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import DevicesIcon from '@material-ui/icons/Devices';
@@ -47,62 +43,7 @@ class NowPlayingBar extends React.Component {
         return (
             <NowPlayingWrapper>
                 <div className="current-track-action-bar">
-                    <CurrentTrackSection>
-                        {currentTrack ?
-                        <React.Fragment>
-                            <div className="current-track-wrapper">
-                                <div className="current-track-cover-art">
-                                    <div className="cover-art-wrapper" >
-                                        <img 
-                                            alt="album-cover" 
-                                            className="track-cover-art"  
-                                            src={currentTrack.track.album.images[2].url} 
-                                        />
-                                    </div>
-                                    <div className="ellipsis-text-wrapper">
-                                        <EllipsisText color="white">
-                                            <span>
-                                                <a>
-                                                    {currentTrack.track.name}
-                                                </a>
-                                            </span>
-                                        </EllipsisText>
-                                        <EllipsisText size={12}>
-                                            <span>
-                                                <a>
-                                                    {currentTrack.track.artists.length > 1 ? 
-                                                    currentTrack.track.artists.map((artist, idx) => currentTrack.track.artists.length - 1 === idx ? (
-                                                        `${artist.name} `
-                                                    ) : `${artist.name}, `)
-                                                    :
-                                                    currentTrack.track.artists[0].name}
-                                                </a>
-                                            </span>
-                                        </EllipsisText>
-                                    </div>
-                                </div>
-                            </div>
-                            <div style={{ 
-                                display: 'flex', 
-                                alignItems: 'center',
-                                justifyContent:'center',
-                                maxWidth: '80px',
-                                height: '30px'
-                            }}>
-                                <div style={{ display: 'flex', justifyContent: 'center', width: '32px' }}>
-                                    <span>
-                                        <LikeIcon style={{ width: '17px '}} />
-                                    </span>
-                                </div>
-                                <div style={{ display: 'flex', justifyContent: 'center', width: '32px' }}>
-                                    <span>
-                                        <PictureInPictureAltIcon style={{ width: '17px '}} />
-                                    </span>
-                                </div>
-                            </div>
-                        </React.Fragment>
-                        : null}
-                    </CurrentTrackSection>
+                    <CurrentTrack currentTrack={currentTrack} />
                     <TrackPlayerSection
                         onPlay={() => this.props.onPlay(currentTrack)} 
                         onPause={this.props.onPause} 
