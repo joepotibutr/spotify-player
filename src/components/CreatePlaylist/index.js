b1e82084b5ab296b17d99cf61eec0f5461e7b88d
vchrpng <vchrpng@gmail.com>
2020-07-01 15:05:10 +0700

    feat(CreatePlaylist): create new component

---
+++ src/components/CreatePlaylist/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { CreatePlaylistWrapper } from './styled'
+
+const modalRoot = document.getElementById("modal-root")
+
+function CreatePlaylist({ onClose }) {
+    const [open,handleToggle] = React.useState(false)
+
+    React.useEffect(() => {
+        window.addEventListener('keydown', onEscKeyDown)
+        setTimeout(() => handleToggle(true), 0)
+        return () => {
+            window.removeEventListener('keydown',onEscKeyDown, false)
+        }
+    },[isOpen])
+
+    function onEscKeyDown(e) {
+        if (e.key === "Escape") {
+            onClose(false)
+        }
+    }
+
+    return ReactDOM.createPortal(
+        <CreatePlaylistWrapper isOpen={open}>
+            Create Playlist
+        </CreatePlaylistWrapper>
+    ,modalRoot)
+}
+
+export default CreatePlaylist
