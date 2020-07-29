67f9279d1027b22598213fe24d4952266ad0d1e4
vchrpng <vchrpng@gmail.com>
2020-07-29 16:04:23 +0700

    style(CreatePlaylist): style  modal

--- src/components/CreatePlaylist/styled.js
+++ src/components/CreatePlaylist/styled.js
@@ -10,8 +10,19 @@ export const CreatePlaylistWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     opacity: ${props => props.isOpen ? 1 : 0};
     z-index: 99;
-    margin: 40px auto;
-    background: rgba(0, 0, 0, 0.5);
+    background: rgba(0, 0, 0, 0.7);
+    color: rgb(255,255,255,1);
+
+
+    .create-playlist-input {
+        width: 100%;
+        background: rebeccapurple;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
 `
